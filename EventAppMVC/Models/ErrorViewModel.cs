900ba8e8f3c9f778bad62d236fffda4b5df0a957
seyyitatim <seyyitatim@gmail.com>
2023-10-08 12:48:49 +0300

    add new mvc project

---
+++ EventAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EventAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
